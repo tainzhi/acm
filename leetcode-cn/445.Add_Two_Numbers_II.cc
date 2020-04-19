10b488e16d01e07581773913aa30fec72eb6fd9f
tainzhi <qfq61@qq.com>
2020-04-19 09:45:11 +0800

    leetcode-cn: 445 使用stack解法”

--- leetcode-cn/445.Add_Two_Numbers_II.cc
+++ leetcode-cn/445.Add_Two_Numbers_II.cc
@@ -6,23 +6,46 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
         stack<int> s1, s2;
-        while (l1 != NULL) s1.push(l1->val);
-        while (l2 != NULL) s2.push(l2->val);
+        while (l1 != NULL)
+        {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while (l2 != NULL) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
         int n1, n2, low_bit = 0;
-        ListNode * head = NULL;
-        while ((!s1.empty() || !s2.empty()) ){
+        ListNode *head = NULL;
+        while ((!s1.empty() || !s2.empty()))
+        {
             n1 = 0;
             n2 = 0;
-            if (!s1.empty()) {n1 = s1.top(); s1.pop();}
-            if (!s2.empty()) {n2 = s2.top(); s2.pop();}
+            if (!s1.empty())
+            {
+                n1 = s1.top();
+                s1.pop();
+            }
+            if (!s2.empty())
+            {
+                n2 = s2.top();
+                s2.pop();
+            }
             n1 = n1 + n2 + low_bit;
             auto node = new ListNode(0);
             node->val = n1 % 10;
             low_bit = n1 / 10;
+            node->next = head;
+            head = node;
+        }
+        if (low_bit == 1) {
+            auto node = new ListNode(1);
             node->next = head;
             head = node;
         }
