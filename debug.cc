d4ebb4c56b2538c1e335a1c96b351569f3a45cfd
tainzhi <qfq61@qq.com>
2020-05-10 19:09:04 +0800

    add debug.cc

---
+++ debug.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <exception>
+#include <map>
+#include <list>
+#include <algorithm>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+#include <sstream>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
